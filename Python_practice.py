9b931ab84e909242d673b7ba80e114be6a8c268a
tshanedc <77986734+tshanedc@users.noreply.github.com>
2021-03-14 13:37:16 -0400

    Add files via upload

---
+++ Python_practice.py
@@ -0,0 +1,4 @@
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+for  county,voters in counties_dict.items():
+    print (county, "County has", voters, "registered voters")
